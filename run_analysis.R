5885fdc38a923f5c76dd9e81f3a0367e9acefb81
alexfromnsk <alex.from.nsk@gmail.com>
2015-09-27 22:54:40 +0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,7 @@ library("reshape2")
 # 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 #I have already unzipped and placed files in my working directory
-setwd("C:/Users/Alex/Documents/R-projects/getdata")
+#setwd("C:/Users/Alex/Documents/R-projects/getdata")
 
 #Load information in data.frames
 x_train <- read.table("train/X_train.txt")
